ee70348af73cdc131aad46fb15d057bfe7410708
Juan Francisco Miranda Aguilar <juanfrancisco.miranda@ezentis.com>
2019-11-06 13:13:10 +0100

    Corregido problema con el fichero de build.
    Signed-off-by: Juan Francisco Miranda Aguilar <juanfrancisco.miranda@ezentis.com>

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-echo "Changing to /src directory..."
-cd src
 echo "Executing MSBuild DLL begin command..."
-dotnet ../tools/sonar/SonarScanner.MSBuild.dll begin /o:"zpecter-github" /k:"Zpecter_TrisvagoHotels" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+dotnet tools/sonar/SonarScanner.MSBuild.dll begin /o:"zpecter-github" /k:"Zpecter_TrisvagoHotels" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build TrisvagoHotels.sln
 echo "Executing MSBuild DLL end command..."
