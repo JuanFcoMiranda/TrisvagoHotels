bb98b2f97c2b170415baeccc08bd31635516ce2e
Juan Francisco Miranda Aguilar <juanfrancisco.miranda@ezentis.com>
2019-11-21 07:47:51 +0100

    Se añade el archivo de extensiones que faltaba.
    Signed-off-by: Juan Francisco Miranda Aguilar <juanfrancisco.miranda@ezentis.com>

---
+++ src/TrisvagoHotels.DataContext/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TrisvagoHotels.DataContext.Context;
+
+namespace Microsoft.Extensions.DependencyInjection {
+	public static class ServiceCollectionExtensions {
+		public static IServiceCollection AddEntityFrameworkCore(this IServiceCollection services, IConfiguration configuration) =>
+			services
+				.AddDbContext<MyDataContext>(options => {
+					options.UseMySql(configuration.GetConnectionString("DataAccessMySqlProvider"));
+				});
+	}
+}
