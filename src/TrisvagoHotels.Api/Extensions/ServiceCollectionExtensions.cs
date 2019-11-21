7173f33958a3fe64fe36dabe2b3ad707b5a78780
Juan Francisco Miranga Aguilar <jfmiranda75@hotmail.com>
2019-11-21 19:01:28 +0100

    Corregidos algunos espacios de nombre y la forma de obtener la cadena de conexión.

--- src/TrisvagoHotels.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/TrisvagoHotels.Api/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
 using TrisvagoHotels.Api.Filters;
 
 namespace Microsoft.Extensions.DependencyInjection {
--- src/TrisvagoHotels.DataContext/Extensions/ServiceCollectionExtensions.cs
+++ src/TrisvagoHotels.DataContext/Extensions/ServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Extensions.DependencyInjection {
 		public static IServiceCollection AddEntityFrameworkCore(this IServiceCollection services, IConfiguration configuration) =>
 			services
 				.AddDbContext<MyDataContext>(options => {
-					options.UseMySql(configuration.GetConnectionString("DataAccessMySqlProvider"));
+					options.UseMySql(configuration["AppSettings:ConnectionStrings:DataAccessMySqlProvider"]);
 				});
 	}
 }
