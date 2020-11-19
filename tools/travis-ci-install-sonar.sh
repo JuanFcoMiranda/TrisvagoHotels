f9bef63c5ec714eb8d570c76c3f4d28e64127ce2
Juan Francisco Miranga Aguilar <jfmiranda75@hotmail.com>
2020-11-19 21:12:55 +0100

    Actualizado archivo de script para instalar sonar.

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -4,4 +4,4 @@ wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/
 echo "Unzipping..."
 unzip -qq sonar.zip -d tools/sonar
 echo "Changing permissions..."
-chmod +x tools/sonar-scanner-msbuild-5.0.4.24009-net5.0/sonar-scanner-4.4.0.2170/bin/sonar-scanner
+chmod +x tools/sonar/sonar-scanner-msbuild-5.0.4.24009-net5.0/sonar-scanner-4.4.0.2170/bin/sonar-scanner
