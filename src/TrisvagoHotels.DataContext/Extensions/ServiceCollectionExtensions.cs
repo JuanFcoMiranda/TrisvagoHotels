a7b174e90a9c19ea11a4da7c9a716ca710c00fbb
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2020-11-28 11:52:48 +0100

    Se corrige la conexión de MySql con Pomelo.

--- src/TrisvagoHotels.DataContext/Extensions/ServiceCollectionExtensions.cs
+++ src/TrisvagoHotels.DataContext/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using TrisvagoHotels.DataContext.Context;
 
 namespace Microsoft.Extensions.DependencyInjection {
@@ -8,7 +9,15 @@ namespace Microsoft.Extensions.DependencyInjection {
 		public static IServiceCollection AddEntityFrameworkCore(this IServiceCollection services, IConfiguration configuration) =>
 			services
 				.AddDbContext<MyDataContext>(options =>
-					options.UseMySql(configuration["AppSettings:ConnectionStrings:DataAccessMySqlProvider"]), ServiceLifetime.Transient);
+					options.UseMySql(configuration["AppSettings:ConnectionStrings:DataAccessMySqlProvider"], 
+							// new MySqlServerVersion(new Version(8, 0, 22)), // use MariaDbServerVersion for MariaDB
+							MySqlServerVersion.LatestSupportedServerVersion,
+							mySqlOptions => mySqlOptions
+								.CharSetBehavior(CharSetBehavior.NeverAppend))
+						// Everything from this point on is optional but helps with debugging.
+						.EnableSensitiveDataLogging()
+						.EnableDetailedErrors(),
+					ServiceLifetime.Transient);
 		
 		public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services, IConfiguration configuration) =>
 			services.AddHealthChecks()
