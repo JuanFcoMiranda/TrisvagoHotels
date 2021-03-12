368c185d3f63f2e802159f195cb5d913e2f4904a
Juan Francisco <jfmiranda@gmail.com>
2021-03-12 19:50:49 +0100

    Update travis-ci-build.sh

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -3,5 +3,7 @@ echo "Executing MSBuild DLL begin command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"zpecter-github" /k:"Zpecter_TrisvagoHotels" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
 dotnet build TrisvagoHotels.sln
+echo "Running tests..."
+dotnet test TrisvagoHotels.sln
 echo "Executing MSBuild DLL end command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
