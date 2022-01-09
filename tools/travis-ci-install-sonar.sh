9e3728b65698588d4117f2a500d4836ef927a671
Juan Francisco Miranga Aguilar <jfmiranda75@hotmail.com>
2022-01-09 12:43:08 +0100

    Corregidas las versiones para travis y sonarcloud

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting install..."
-wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.2.1.31210/sonar-scanner-msbuild-5.2.1.31210-net5.0.zip
+wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.4.1.41282/sonar-scanner-msbuild-5.4.1.41282-net5.0.zip
 echo "Unzipping..."
 unzip -qq sonar.zip -d tools/sonar
 echo "Changing permissions..."
