c5e09f456cd8a767dee40b90d17d4315604ee08a
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2025-08-07 09:54:40 +0200

    Updated some references

--- src/TrisvagoHotels.Api/Validations/ValidationBehavior.cs
+++ src/TrisvagoHotels.Api/Validations/ValidationBehavior.cs
@@ -12,7 +12,8 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
 
     public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => this.validators = validators;
 
-    public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next) {
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
         var context = new ValidationContext<TRequest>(request);
         var failures = validators
             .Select(v => v.Validate(context))
